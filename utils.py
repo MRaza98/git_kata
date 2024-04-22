9878da2f0ffa2acbdbda2040dc039a30af9e47c1
fredmepru97 <fredmepru97@gmail.com>
2024-04-22 15:00:47 +0200

    male top 10 returned

--- utils.py
+++ utils.py
@@ -4,6 +4,6 @@ def get_females(dataset_path):
 
     df = pd.read_csv(dataset_path)
 
-    female_passengers = df[df['sex']  == 'female']
+    female_passengers = df[df['sex']  == 'male']
 
     return(female_passengers)
