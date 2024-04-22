689cd72427ba2695b1d32e123172aed0a0686620
MRaza98 <raza.1998@hotmail.com>
2024-04-22 14:01:18 +0200

    added function

--- utils.py
+++ utils.py
@@ -1,1 +1,13 @@
 import pandas as pd
+
+def get_females(dataset_path):
+
+    df = pd.read_csv(dataset_path)
+
+    female_passengers = df[df['sex']  == 'female']
+
+    return(female_passengers)
+
+dataset_path = '/Users/muhammadraza/Documents/GitHub/git_kata/data/titanic.csv'
+female_names = get_females(dataset_path)
+print(female_names)
