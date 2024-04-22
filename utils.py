29aa5a4c745ca8b1e190286cfd9d95b79edc9336
MRaza98 <raza.1998@hotmail.com>
2024-04-22 14:58:11 +0200

    function edited in utils and eda

--- utils.py
+++ utils.py
@@ -7,7 +7,3 @@ def get_females(dataset_path):
     female_passengers = df[df['sex']  == 'female']
 
     return(female_passengers)
-
-dataset_path = '/Users/muhammadraza/Documents/GitHub/git_kata/data/titanic.csv'
-female_names = get_females(dataset_path)
-print(female_names)
